55b762c27093d9f6af54247924f28126b28a3ed6
Ana Júlia Gomes <106327639+wtfanaju@users.noreply.github.com>
2024-09-20 11:02:53 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,13 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+
+async function vizualiazarInformacoesGlobai() {
+    const res = await fetch(url)
+    const dados = await res.json() 
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados;total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_médio} horas conectadas.'
+    console.log(paragrafo)
+}
+
+vizualiazarInformacoesGlobais()
